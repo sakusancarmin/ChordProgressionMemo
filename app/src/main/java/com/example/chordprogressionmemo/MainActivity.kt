8ce3211bfbd6493c044d8df7206dc27eff0cf6a7
sakusancarmin <6679228+sakusancarmin@users.noreply.github.com>
2024-02-18 14:25:29 +0900

    リストのアイテムにOnClickイベントを追加した（クリック時の処理はダミー）

--- app/src/main/java/com/example/chordprogressionmemo/MainActivity.kt
+++ app/src/main/java/com/example/chordprogressionmemo/MainActivity.kt
@@ -4,20 +4,27 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,6 +52,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ChordListView()
 {
+    var selectedText by remember { mutableStateOf("") }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -58,14 +67,32 @@ fun ChordListView()
             )
         }
     ) {
-        ChordList(it)
+        ChordList(it) { name ->
+            // リストクリック時の処理
+            selectedText = name
+        }
     }
+
+    if (selectedText == "") {
+        return
+    }
+
+    // TODO: ボタンクリック時に別画面に遷移させる
+    AlertDialog(
+        onDismissRequest = { /*TODO*/ },
+        confirmButton = {
+            TextButton(onClick = { selectedText = "" }) {
+                Text("OK")
+            }
+        },
+        text = { Text("$selectedText is clicked.") }
+    )
 }
 
 
 @Composable
-fun ChordList(topBarPadding: PaddingValues) {
-    // ToDo: データベースからリストを生成する
+fun ChordList(topBarPadding: PaddingValues, onClickItem: (String)->Unit = {}) {
+    // TODO: データベースからリストを生成する
     val testList = listOf("a", "b", "c")
 
     LazyColumn(
@@ -75,7 +102,8 @@ fun ChordList(topBarPadding: PaddingValues) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(20.dp),
+                    .clickable { onClickItem(it) }
+                    .padding(20.dp)
             ) {
                 Text(
                     text = it,
@@ -90,3 +118,4 @@ fun ChordList(topBarPadding: PaddingValues) {
         }
     }
 }
+
