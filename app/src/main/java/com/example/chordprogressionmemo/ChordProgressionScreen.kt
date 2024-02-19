f00beed16f6c0e8bfcaf907aaec17fad6d71d33a
sakusancarmin <6679228+sakusancarmin@users.noreply.github.com>
2024-02-20 08:01:17 +0900

    音名に関するEnum型を定義し、試験的に音名のドロップダウンリストを画面に追加した

--- app/src/main/java/com/example/chordprogressionmemo/ChordProgressionScreen.kt
+++ app/src/main/java/com/example/chordprogressionmemo/ChordProgressionScreen.kt
@@ -1,17 +1,27 @@
 package com.example.chordprogressionmemo
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,6 +48,46 @@ fun ChordProgressionScreen(itemName: String = "", onClick: () -> Unit = {}) {
             )
         }
     ) {
+        var expanded by remember { mutableStateOf(false) }
+        var selectedOptionText by remember { mutableStateOf("") }
+
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = it },
+            modifier = Modifier.padding(it)
+        ) {
+            val noteOptions = NoteName.entries
+
+            TextField(
+                modifier = Modifier.menuAnchor(),
+                readOnly = true,
+                value = selectedOptionText,
+                onValueChange = {},
+                label = { Text("根音") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                colors = ExposedDropdownMenuDefaults.textFieldColors()
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                noteOptions.forEach {
+                    DropdownMenuItem(
+                        text = { Text(it.displayName()) },
+                        onClick = {
+                            selectedOptionText = it.displayName()
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                    )
+
+                }
+
+            }
+
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/chordprogressionmemo/Note.kt
@@ -0,0 +1,57 @@
+package com.example.chordprogressionmemo
+
+
+// 変化記号をまとめたもの
+// valueには、BasicNoteNameに対する音の変化量を設定している(半音を1とする)
+enum class AccidentalMark(val value: Int, val displayName: String)
+{
+    SHARP(1,"♯"),
+    FLAT(-1,"♭"),
+    NONE(0,"")
+}
+
+// 変化記号なしの音名をまとめたもの
+// valueには、C = 0としたときの相対値を定義(半音を1とする)
+enum class BasicNoteName(val value: Int, val displayName: String) {
+    C(0, "C"),
+    D(2, "D"),
+    E(4, "E"),
+    F(5, "F"),
+    G(7, "G"),
+    A(9, "A"),
+    B(11, "B");
+}
+
+// 変化記号含む音名をまとめたもの
+enum class NoteName(val noteName: BasicNoteName, val accidental: AccidentalMark = AccidentalMark.NONE)
+{
+    C(BasicNoteName.C),
+    C_SHARP(BasicNoteName.C, AccidentalMark.SHARP),
+    D_FLAT(BasicNoteName.D, AccidentalMark.FLAT),
+    D(BasicNoteName.D),
+    D_SHARP(BasicNoteName.D, AccidentalMark.SHARP),
+    E_FLAT(BasicNoteName.E, AccidentalMark.FLAT),
+    E(BasicNoteName.E),
+    F(BasicNoteName.F),
+    F_SHARP(BasicNoteName.F, AccidentalMark.SHARP),
+    G_FLAT(BasicNoteName.G, AccidentalMark.FLAT),
+    G(BasicNoteName.G),
+    G_SHARP(BasicNoteName.G, AccidentalMark.SHARP),
+    A_FLAT(BasicNoteName.A, AccidentalMark.FLAT),
+    A(BasicNoteName.A),
+    A_SHARP(BasicNoteName.A, AccidentalMark.SHARP),
+    B_FLAT(BasicNoteName.B, AccidentalMark.FLAT),
+    B(BasicNoteName.B);
+
+    fun value() :Int
+    {
+        val value = noteName.value + accidental.value
+        assert(value >= 0 && value < 12)
+        return value
+    }
+
+    fun displayName() :String
+    {
+        return noteName.displayName + accidental.displayName
+    }
+}
